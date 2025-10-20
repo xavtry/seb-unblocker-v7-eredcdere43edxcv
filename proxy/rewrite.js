2088cd85342b49779677f7474fa9b7d1905959e7
xavtry <sebastianishot68@gmail.com>
2025-10-20 18:34:30 -0400

    Update rewrite.js

--- proxy/rewrite.js
+++ proxy/rewrite.js
@@ -1,1 +1,40 @@
+const { URL } = require('url');
 
+class Rewriter {
+  rewriteHTML(html, baseUrl) {
+    // Rewrite href/src
+    return html.replace(/(href|src)=["']([^"']+)["']/g, (match, attr, link) => {
+      try {
+        const absolute = new URL(link, baseUrl).href;
+        return `${attr}="/proxy?url=${encodeURIComponent(absolute)}"`;
+      } catch {
+        return match;
+      }
+    });
+  }
+
+  rewriteCSS(css, baseUrl) {
+    return css.replace(/url\(["']?([^"')]+)["']?\)/g, (match, url) => {
+      try {
+        const absolute = new URL(url, baseUrl).href;
+        return `url("/proxy?url=${encodeURIComponent(absolute)}")`;
+      } catch {
+        return match;
+      }
+    });
+  }
+
+  rewriteJS(js, baseUrl) {
+    // Simple JS rewriting (fetch/axios URLs)
+    return js.replace(/(['"`])((https?:)?\/\/[^'"`]+)\1/g, (match, quote, url) => {
+      try {
+        const absolute = new URL(url, baseUrl).href;
+        return `${quote}/proxy?url=${encodeURIComponent(absolute)}${quote}`;
+      } catch {
+        return match;
+      }
+    });
+  }
+}
+
+module.exports = new Rewriter();
