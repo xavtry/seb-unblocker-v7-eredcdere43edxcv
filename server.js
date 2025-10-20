f3bceca0b2df8d6b34f9edcb4867af255347c312
xavtry <sebastianishot68@gmail.com>
2025-10-20 18:33:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const setup = require('./setup');
+const proxyMiddleware = require('./proxy/proxyMiddleware');
+const logger = require('./proxy/logger');
+const errorPage = require('./proxy/errorPage');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Setup folders/logs
+setup.init();
+
+// Serve static frontend
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Main proxy route
+app.use('/proxy', proxyMiddleware);
+
+// Root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Error fallback
+app.use((err, req, res, next) => {
+  logger.logError(err, req.originalUrl);
+  res.status(500).send(errorPage.renderErrorPage(err.message));
+});
+
+app.listen(PORT, () => {
+  console.log(`Seb-Unblocker-V7 running at http://localhost:${PORT}`);
+});
