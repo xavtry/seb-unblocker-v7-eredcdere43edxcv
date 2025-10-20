6d0bd6b47b083281544bff0e8745fcedc3726f95
xavtry <sebastianishot68@gmail.com>
2025-10-20 18:33:46 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,1 +1,13 @@
 
+const fs = require('fs');
+const path = require('path');
+
+module.exports.init = () => {
+  const folders = ['logs', 'proxy', 'public/css', 'public/js', 'public/images', 'public/fonts', 'views'];
+  folders.forEach(f => {
+    if (!fs.existsSync(f)) fs.mkdirSync(f, { recursive: true });
+  });
+  const logFile = path.join('logs', 'proxy.log');
+  if (!fs.existsSync(logFile)) fs.writeFileSync(logFile, '', 'utf8');
+  console.log('Setup complete: folders and logs initialized.');
+};
